88b4994fb9181ab62045ebba88ad41899f683743
OLIX <80951658+OLIX-boop@users.noreply.github.com>
2022-02-22 17:21:00 +0100

    Updated client.lua
    - added compatibility with esx.notifications

--- ol-gps/client.lua
+++ ol-gps/client.lua
@@ -1,25 +1,25 @@
 -----------------------------------------------------------
 ------------ Made By 𝓞𝓛𝓘𝓧 𝓨𝓣 (っ◔◡◔)っ#4222 ------------
 -----------------------------------------------------------
+ESX = nil
 
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
 
 RegisterCommand("gps", function(source, args )
     local argString = table.concat(args, " ")
     if argString == "" or argString == " " then 
-        notify("Mettere un numero dopo il comando.")
+        ESX.ShowNotification("Mettere un numero dopo il comando.")
     end
     if GPS[argString] then
         local coord = GPS[argString]
-        notify("GPS Impostato al civico ~g~" .. argString)
-        SetNewWaypoint(coord[1], coord[2])
+        ESX.ShowNotification("GPS impostato al civico "..argString)
+        SetNewWaypoint(coord.x, coord.y)
     else 
-        notify("Civico non trovato.")
+        ESX.ShowNotification("Civico non trovato.")
     end
 end)
-
-
-function notify(string)  -- Metti le tue notifiche o lascia queste
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(string)
-    DrawNotification(true, false)
-end
